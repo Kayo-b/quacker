a1834eaaf57463de770d9eb2825263408244c456
kayo-b <kyo.bhz@gmail.com>
2023-04-12 23:53:38 +0200

    figuring out file structure, routes and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import Homepage from './components/Homepage'
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
 import logo from './logo.svg';
 import './App.css';
 import Bookmarks from './components/Bookmarks';
 
 function App() {
   return (
+    <div className="App">
     <BrowserRouter>
-      <Sidebar/>
+      <Sidebar />
+      <div className="center-container">
+      <Navbar />
       <Routes>
         <Route
         path="/homepage/"
@@ -20,7 +24,10 @@ function App() {
         element={<Bookmarks/>}
         />
       </Routes>
+      </div>
     </BrowserRouter>
+    </div>
+
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+    return(
+        <div className="nav-main-container">
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
