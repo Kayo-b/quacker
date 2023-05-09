dfe30d3c5b4c171db1db85660ae16e9ef5a42819
Kayo-b <kyo.bhz@gmail.com>
2023-05-09 22:36:44 +0200

    adding response structure

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { DocumentData } from 'firebase/firestore'
-
+import { DocumentData } from 'firebase/firestore';
+import myImg from '../img/user-icon.png';
 
 type PostProps = {
     setNewPost: React.Dispatch<React.SetStateAction<DocumentData[]>>;
@@ -9,13 +9,35 @@ type PostProps = {
     update: undefined | boolean;
     setUpdate: React.Dispatch<React.SetStateAction<boolean | undefined>>;
 }
+
+
+        
 const Post: React.FC<PostProps> = ({ update ,newPost , posts, setUpdate }) => {
 
     let neuPost = newPost.map(post =>
-         <li key={post.id}>{post.textContent} <br></br>{post.userName}</li>
-         )
+      <li key={post.id}>{post.textContent} <br></br><i style={{fontSize: "12px"}}>
+      by {post.userName}
+      </i></li>
+    )
     let loadPosts = posts.map(post => 
-      <li key={post.id}>{post.textContent} <br></br>{post.username}</li>
+      <div className="post-container">
+        <div className="user-container">
+          <img className="profile-picture" alt="user icon" src={myImg}></img>
+          <span>
+            <div className="user-name">
+              {post.username}
+            </div>
+          <div className="content">
+            <li key={post.id}>
+              {post.textContent}
+            </li>
+          </div>
+          </span>
+          
+        </div>
+       
+      </div>
+
     )
     
   return (
@@ -23,9 +45,6 @@ const Post: React.FC<PostProps> = ({ update ,newPost , posts, setUpdate }) => {
       <div>{ neuPost }</div>
       <div>{ loadPosts }</div>
     </div>
-    
-    
-   
   )
   
 }
