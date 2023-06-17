fafc29274c42a925fd2aaca4867177e4a4951a49
Kayo-b <kyo.bhz@gmail.com>
2023-06-17 18:46:26 +0200

    created the logic to handle when the user reposts its own post

--- src/components/Repost.tsx
+++ src/components/Repost.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { DocumentData, arrayUnion, arrayRemove, doc, setDoc , getDoc, collection, where, query, getDocs} from "firebase/firestore"
 import { db } from "../firebase";
+import { count } from 'console';
 
 type UserProps = {
     authProvider?: string;
@@ -83,7 +84,7 @@ const Repost: React.FC<PostProps> = ({
 
         if(setUserMainFeed) {
             setUserMainFeed(
-            prevVal => [...prevVal, post?.postID]) 
+              prevVal => [...prevVal, post?.postID]) 
         }
         
       }
@@ -91,6 +92,9 @@ const Repost: React.FC<PostProps> = ({
       
       //See if there is a faster way to get the user's bookmarked posts, the query makes it take some time
     async function addRepost(postId: string) {
+
+          console.log(post)
+          
           // const q = query(collection(db, 'users'), where("uid", "==",  user.uid));
           // const docs = await getDocs(q);
           // const userRef = docs.docs[0].ref;
@@ -102,12 +106,27 @@ const Repost: React.FC<PostProps> = ({
         //       mainFeed: arrayUnion(post?.postID)
         //   }, {merge: true})
           
-          //const userDoc = await getDoc(userRef);
+          const userDoc = await getDoc(userRef);
           //const userData = userDoc.data();
           if(!reposted) {
                 setReposted(true);
+                // if(post?.userID !== user.uid){
+                //   setDoc(userRef, {reposts: arrayUnion(postId)}, {merge: true});
+                // }
                 setDoc(userRef, {reposts: arrayUnion(postId)}, {merge: true});
-                setDoc(userRef, {mainFeed: arrayUnion(postId)}, {merge: true});
+                if(post?.userID === user.uid) {
+                  console.log(userDoc.exists())
+                  if(userDoc.exists()) {
+                    const userData = userDoc.data();
+                    const mainFeed = userData.mainFeed;
+                    mainFeed.push(postId);
+                    setDoc(userRef, {mainFeed: mainFeed}, {merge: true});
+                  }
+
+                } else {
+                  setDoc(userRef, {mainFeed: arrayUnion(postId)}, {merge: true});
+                  
+                }
                 setDoc(postRef, {repostByUsers: arrayUnion(user.uid)}, {merge: true});
                 setUpdate(true);
                 if(post) addRepostPost(post);
@@ -115,8 +134,29 @@ const Repost: React.FC<PostProps> = ({
 
           } else {
                 setReposted(false);
+                //handling when the user reposts its own post
+                if(userDoc.exists()) {
+                  const userData = userDoc.data();
+                  const mainFeed = userData.mainFeed;
+                  //Getting the indexes of the unreposted posts that have the same ID
+                  const indexArray = mainFeed.reduce((acc: Array<number>, item: string, index: number) => {
+                    if(item === postId) {
+                      acc.push(index)
+                    }
+                    return acc;
+                  },[])
+                  //If length of resulting array is > than 1, that means that we are dealing with a "self reposted" post
+                  //that means that there are two posts in the main feed with the same ID, the second post(with higher index value)
+                  //will be the reposted post, this is the one to be removed so that the order of posts can been correctly maintained
+                  if(indexArray.length > 1) {
+                    mainFeed.splice(indexArray[1], 1)
+                    setDoc(userRef, {mainFeed: mainFeed}, {merge: true})
+                    
+                  } else {
+                    setDoc(userRef, {mainFeed: arrayRemove(postId)}, {merge: true});
+                  }
+                }
                 setDoc(userRef, {reposts: arrayRemove(postId)}, {merge: true});
-                setDoc(userRef, {mainFeed: arrayRemove(postId)}, {merge: true});
                 setDoc(postRef, {repostByUsers: arrayRemove(user.uid)}, {merge: true});
                 setUpdate(false);
                 removeRepost(post?.postID);
